453a8494659eb168bf30fc429bea6fbc3d1d515e
Raadwal <76631231+Raadwal@users.noreply.github.com>
2022-05-17 20:02:24 +0200

    Models added
    Course model and Material model were added.

--- backend/paczka/models.py
+++ backend/paczka/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
@@ -5,8 +6,28 @@ class Lecturer(models.Model):
     name = models.CharField(max_length=80)
     surname = models.CharField(max_length=80)
 
+
     def __str__(self):
         return self.name + " " + self.surname
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=80)
+    description = models.TextField()
+    obligatory = models.BooleanField()
+    semester = models.IntegerField(validators=[MaxValueValidator(7), MinValueValidator(1)])
+
+    lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE, blank=False, null=False)
+
+
+    def __str__(self):
+        if self.obligatory:
+            obligatory_text = 'obligatory'
+        else:
+            obligatory_text = 'non obligatory'
+
+        return 'Course ' + self.course_name + ' is taught by ' + self.lecturer.__str__() +\
+            ' at '+ self.semester + ' and is ' + obligatory_text + ": " + self.description
+
 
 class Review(models.Model):
     author_name = models.CharField(max_length=80)
@@ -16,7 +37,7 @@ class Review(models.Model):
     description = models.TextField()
 
     lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE, blank=True, null=True)
-    #course = models.ForeignKey(Course, on_delete=models.CASCADE, blank=True, null=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, blank=True, null=True)
 
 
     def __str__(self):
@@ -27,3 +48,20 @@ class Review(models.Model):
         return 'Review by ' + self.author_name + ' ' + self.author_surname + '(' + self.author_email + ')' +\
                ' about ' + about.__str__() + ' with score ' + str(self.score) + ': ' + self.description
 
+
+class Material(models.Model):
+    author_name = models.CharField(max_length=80)
+    author_surname = models.CharField(max_length=80)
+    author_email = models.EmailField()
+    path_to_file = models.CharField(max_length=160)
+    descripton = models.TextField()
+    upload_date = models.DateField()
+
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, blank=False, null=False)
+
+    def __str__(self):
+        return 'Uploaded by ' + self.author_name + ' ' + self.author_surname + '(' + self.author_email + ')' +\
+            ' at ' + self.upload_date + ': ' + self.descripton
+
+    def get_path_to_file(self):
+        return self.path         
