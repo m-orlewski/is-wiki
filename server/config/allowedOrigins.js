65681e0df96ecf6636356ed84411490e84bf6658
9orlewski <morlewski@student.agh.edu.pl>
2022-05-23 22:21:10 +0200

    merge

--- server/config/allowedOrigins.js
+++
@@ -1,9 +0,0 @@
-const allowedOrigins = [
-    'https://www.yoursite.com',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://localhost:3000',
-    'https://is-wiki.herokuapp.com'
-];
-
-module.exports = allowedOrigins;
