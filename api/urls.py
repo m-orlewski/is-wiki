ca51fdd66b6b972a5e8a740f2439256a5aebd533
9orlewski <morlewski@student.agh.edu.pl>
2022-06-17 18:34:21 +0200

    add urlpatter

--- api/urls.py
+++ api/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api/', include('paczka.urls')),
     path('api/user/', include('users.urls', namespace='users')),
