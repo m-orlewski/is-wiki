648eb14e491263e2e5c87621e8fc6875a3314cae
9orlewski <morlewski@student.agh.edu.pl>
2022-06-17 18:15:46 +0200

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // add when push on heroku
-// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
@@ -11,7 +11,9 @@ import { createRoot } from "react-dom/client";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
+
 // delete when push on heroku
+/*
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -23,17 +25,18 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+*/
 
 // add when push on heroku
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <AuthProvider>
-//         <Routes>
-//           <Route path="/*" element={<App />} />
-//         </Routes>
-//       </AuthProvider>
-//     </BrowserRouter>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ ReactDOM.render(
+   <React.StrictMode>
+     <BrowserRouter>
+       <AuthProvider>
+         <Routes>
+           <Route path="/*" element={<App />} />
+         </Routes>
+       </AuthProvider>
+     </BrowserRouter>
+   </React.StrictMode>,
+   document.getElementById('root')
+ );
