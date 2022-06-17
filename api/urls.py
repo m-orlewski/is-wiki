ff72b7ea2276c3017a5496ee302022c2dc1abde9
9orlewski <morlewski@student.agh.edu.pl>
2022-06-17 18:41:26 +0200

    serve static files

--- api/urls.py
+++ api/urls.py
@@ -17,6 +17,11 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.views.static import serve
+import settings
+
+re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 urlpatterns = [
     re_path('.*', TemplateView.as_view(template_name='index.html')),
